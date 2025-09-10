1cda8e6c45620f4c22338045187be2b731f96e3a
populusLeaves-leaf32 <populus_32@outlook.com>
2025-09-10 15:00:00 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+enum SERVICE
+{
+    NON_OFFICIAL
+};
+
+void numerical_puzzle()
+{
+    bool misery = true;
+    float num = 1.0f;
+    SERVICE service;
+    if (misery)
+    {
+        service = NON_OFFICIAL;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    numerical_puzzle();
+}
